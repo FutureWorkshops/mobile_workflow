9cf47e55faa2a0bd1c1601706ff4fd9683666f9a
Matt Brooke-Smith <matt@futureworkshops.com>
2020-12-27 21:52:47 +0000

    Copy templates and generators from mobile_workflow_cli

---
+++ lib/mobile_workflow/cli.rb
@@ -0,0 +1,9 @@
+require "mobile_workflow/version"
+
+require "mobile_workflow/cli/app_server_generator"
+require "mobile_workflow/cli/app_builder"
+require "mobile_workflow/cli/app_server_cleaner"
+
+require "mobile_workflow/cli/aws_backend"
+require "mobile_workflow/cli/heroku_backend"
+require "mobile_workflow/cli/dokku_backend"
---
+++ lib/mobile_workflow/cli/dokku_backend.rb
@@ -0,0 +1,57 @@
+module MobileWorkflow::Cli
+  class DokkuBackend
+    def initialize(dokku_host:, app_name:)
+      @dokku_host = dokku_host
+      @dokku_app_name = app_name.gsub("_", "-")
+    end
+  
+    def create
+      remote_command "dokku apps:create #{@dokku_app_name}"
+      remote_command "dokku postgres:create #{@dokku_app_name}"
+      remote_command "dokku postgres:link #{@dokku_app_name} #{@dokku_app_name}"
+      remote_command "dokku domains:enable #{@dokku_app_name}"
+      remote_command "dokku letsencrypt #{@dokku_app_name}"
+    
+      local_command "git remote add dokku dokku@#{@dokku_host}:#{@dokku_app_name}"
+    end
+  
+    def configure_activestorage
+ 
+    end
+  
+    def deploy
+      local_command "git push dokku master"
+    end
+  
+    def sync_dotenv
+      env = File.read(".env").split.join(" ")
+      puts "Setting env: #{env}"
+      local_command "dokku config:set #{env}"
+    end
+  
+    def destroy
+      remote_command "dokku apps:destroy #{@dokku_app_name}"
+    end
+  
+    def dokku_app_host
+      remote_command "dokku url #{@dokku_app_name}"
+    end
+  
+    def notifications_endpoint
+      "https://#{dokku_app_host}/sns_notifications"
+    end
+  
+    private
+    def remote_command(command)
+      command = "ssh -t ubuntu@#{@dokku_host} '#{command}'"
+      local_command(command)
+    end
+  
+    def local_command(command)
+      puts "Running: #{command}"
+      output = `#{command}`
+      puts "Output: #{output}" unless output.blank?
+      return output
+    end
+  end
+end
