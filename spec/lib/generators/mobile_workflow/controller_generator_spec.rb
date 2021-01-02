a236c99e40056487998423b7797ee0c4cc479f86
Matt Brooke-Smith <matt@futureworkshops.com>
2021-01-02 12:54:52 +0000

    Move controller generator spec to correct location

--- spec/lib/generators/mobile_workflow/controller_generator_spec.rb
+++ spec/lib/generators/mobile_workflow/controller_generator_spec.rb
@@ -11,6 +11,11 @@ module MobileWorkflow
         before(:each) { allow(subject).to receive(:attributes_names) { ["name", "age"] } }
         it { expect(subject.send(:permitted_params)).to eq ":name, :age" }
       end
+      
+      describe '#copy_controller_and_spec_files' do
+        before(:each) { allow(subject).to receive(:doorkeeper_oauth?) { true } }
+        it { expect(subject.copy_controller_and_spec_files).to_not be_nil }
+      end
     end
   end
 end
