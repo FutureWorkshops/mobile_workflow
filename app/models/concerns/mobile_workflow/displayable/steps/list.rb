48b3f5ab2f985bdfccc48105feccb6a6dafd6ace
Montse <montse@futureworkshops.com>
2021-09-23 18:47:14 +0200

    Update displayable list and grid

--- app/models/concerns/mobile_workflow/displayable/steps/list.rb
+++ app/models/concerns/mobile_workflow/displayable/steps/list.rb
@@ -2,9 +2,9 @@ module MobileWorkflow
   module Displayable
     module Steps
       module List
-        def mw_list_item(id: self.id, text:, detail_text: nil, sf_symbol_name: nil, material_icon_name: nil, image_attachment: nil)
-          mw_list_item = {id: id, text: text, detailText: detail_text, sfSymbolName: sf_symbol_name, materialIconName: material_icon_name}
-          mw_list_item[:imageURL] = preview_url(image_attachment, options: { resize_to_fill: [200, 200] }) if image_attachment
+        def mw_list_item(text:, id: self.id, detail_text: nil, sf_symbol_name: nil, material_icon_name: nil, image_attachment: nil, image_url: nil)
+          mw_list_item = { id: id, text: text, detailText: detail_text, sfSymbolName: sf_symbol_name, materialIconName: material_icon_name }
+          mw_list_item[:imageURL] = image_attachment&.attached? ? preview_url(image_attachment, options: { resize_to_fill: [200, 200] }) : image_url
           mw_list_item.compact
         end
         
--- app/models/concerns/mobile_workflow/displayable/steps/styled_content/grid.rb
+++ app/models/concerns/mobile_workflow/displayable/steps/styled_content/grid.rb
@@ -17,11 +17,11 @@ module MobileWorkflow
             { id: id, text: text, type: :smallSection }
           end
           
-          def mw_grid_item(id: self.id, text:, image_attachment: nil, options: { resize_to_fill: [1560, 877.5] })
+          def mw_grid_item(id: self.id, text:, image_attachment: nil, detail_text: nil, options: { resize_to_fill: [1560, 877.5] })
             raise 'Missing id' if id.nil?
             raise 'Missing text' if text.nil?
             
-            item = { id: id, text: text, type: :item }
+            item = { id: id, text: text, type: :item, detailText: detail_text }
             item[:imageURL] = preview_url(image_attachment, options: options) if image_attachment
             item
           end
