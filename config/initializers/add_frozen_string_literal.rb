818c05db856f382d5c87def0926c7c4339112754
Montse <montse@futureworkshops.com>
2021-10-13 18:48:29 +0200

    Add comment add_frozen_string_literal file

--- config/initializers/add_frozen_string_literal.rb
+++ config/initializers/add_frozen_string_literal.rb
@@ -1,9 +1,9 @@
 # frozen_string_literal: true
 
-# Adds a `frozen_string_literal` comment to the top of files created by Rails
-# generators.
-#
+# Adds a `frozen_string_literal` comment to the top of files created by Rails generators.
 # Taken from https://gist.github.com/thornomad/4e2f0905e2a4a6eefbc4be5772dfd4f7#gistcomment-3533276
+#
+# Warning! Doorkeeper auto generated files already include `frozen_string_literal`, so it will be duplicated.
 
 return unless defined?(::Rails::Generators)
 
